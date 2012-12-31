f14f2873e590c62a10b9786cb9b4818c938dfe7b
Exodus111 <aurelioxxx@hotmail.com>
2012-12-31 17:59:23 +0100

    Fixec background and added scrollable world. Use the mouse to scroll the world in any direction. PROBLEM: Right not this breaks the rotation of the player image.

--- Inf_Adv.py
+++ Inf_Adv.py
@@ -11,52 +11,83 @@ class Game(GameState):
     def __init__(self):
         self.w = 1240
         self.h = 960 
+        self.level_size = (3200, 3200)
         GameState.__init__(self, size=(self.w, self.h), fill=(255, 255, 255))
+        
+        
         self.brickColor = (156, 102, 31)
         self.coldgreyColor = (128, 138, 135)
         self.yellowColor = (255, 255, 0)
-              
-        level1 = [line.strip() for line in open('level_1.txt')]
-        self.walls, self.end_rect = self.draw_map(level1, 32, 32)
         
-        self.mouse_image = pygame.image.load("Red_Sights.png")
+        
+        self.background = pygame.Surface(self.level_size)
+        self.bx_pos, self.by_pos = (0, 0)
+        
+        self.background.fill(self.brickColor)
+        print self.screen.get_size()      
+        level1 = [line.strip() for line in open('level_1.txt')]
+        self.walls, self.end_rect, self.space = self.draw_map(level1, 44, 44)
+        
+        self.mouse_image = pygame.image.load("Red_Sights.png").convert_alpha()
         self.mouse_pos = (0, 0)
         
         self.player_rect = pygame.Rect(132, 132, 38, 38)
-        self.player_image = pygame.image.load("Arrow_cursor.png")
+        self.player_image = pygame.image.load("Arrow_cursor.png").convert_alpha()
         self.player_moveUp, self.player_moveLeft, self.player_moveDown, self.player_moveRight = False, False, False, False
         self.player_rot = 90
-                
+           
                     
         
         
     def update(self):
+        
         if self.player_moveUp == True:
-            self.player_rect.y -= 5
+            self.player_rect.y -= 15
             self.collision(self.walls, self.player_rect, "UP")
         if self.player_moveLeft == True:
-            self.player_rect.x -= 5
+            self.player_rect.x -= 15
             self.collision(self.walls, self.player_rect, "LEFT")
         if self.player_moveDown == True:
-            self.player_rect.y += 5
+            self.player_rect.y += 15
             self.collision(self.walls, self.player_rect, "DOWN")
         if self.player_moveRight == True:
-            self.player_rect.x += 5
+            self.player_rect.x += 15
             self.collision(self.walls, self.player_rect, "RIGHT")
         
         self.playerimg = self.rotate_image(self.player_image, (self.player_rot + 90))
         
+        if self.mouse_pos[0] >= (self.w - (self.w /8)):
+            if (self.bx_pos + self.level_size[0])  != self.w:
+                self.bx_pos = self.bx_pos - 50
+        elif self.mouse_pos[0] <= (self.w /8):
+            if self.bx_pos  != 0:
+                self.bx_pos = self.bx_pos + 50
+        elif self.mouse_pos[1] >= (self.h - (self.h /8)):
+            if (self.by_pos + self.level_size[1])  != self.h:
+                self.by_pos = self.by_pos - 50
+        elif self.mouse_pos[1] <= (self.w /8):
+            if (self.by_pos)  != 0:
+                self.by_pos = self.by_pos + 50 
+        
             
     
     def draw(self):
-        self.screen.fill(self.brickColor)        
-        for wall in self.walls:
-            pygame.draw.rect(self.screen, (self.coldgreyColor), wall)
-        pygame.draw.rect(self.screen, (self.yellowColor), self.end_rect)
+        self.screen.blit(self.background, (self.bx_pos, self.by_pos))
         
-        pygame.draw.rect(self.screen, (255, 255, 255,), self.player_rect, -1)
-        self.screen.blit(self.playerimg, ((self.player_rect.x -3), (self.player_rect.y -3)))
+        for floor in self.space:
+            pygame.draw.rect(self.background, (self.brickColor), floor)
+           
+              
+        for wall in self.walls:
+            pygame.draw.rect(self.background, (self.coldgreyColor), wall)
+    
+            
+        pygame.draw.rect(self.background, (self.yellowColor), self.end_rect)
+        
+        pygame.draw.rect(self.background, (255, 255, 255,), self.player_rect, 1)
+        self.background.blit(self.playerimg, ((self.player_rect.x -3), (self.player_rect.y -3)))
         self.screen.blit(self.mouse_image, self.mouse_pos)
+        
      
     
     def key_down(self, key):
@@ -68,6 +99,10 @@ class Game(GameState):
             self.player_moveDown = True
         if key in (K_d, K_RIGHT):
             self.player_moveRight = True
+        if key == K_ESCAPE:
+            pygame.quit()
+
+            
              
             
     def key_up(self, key):
@@ -89,10 +124,10 @@ class Game(GameState):
     def mouse_motion(self, buttons, pos, rel):
         self.mouse_pos = pos
         self.player_rot = self.rotate((self.player_rect.x, self.player_rect.y), self.mouse_pos)
-       
+             
     
 
             
             
 s = Game()
-s.main_loop(60)
+s.main_loop(120)
