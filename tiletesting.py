c0b1f82189fde63d658edc5fd1a1cabb8d494a9c
Exodus111 <aurelioxxx@hotmail.com>
2013-01-25 16:34:16 +0100

    updated Tiletesting with a much better room generation.

--- tiletesting.py
+++ tiletesting.py
@@ -14,7 +14,7 @@ class main():
     def __init__(self):
         pygame.init()
         screen = pygame.display.set_mode((1024, 960))
-        size = (6400, 6400)
+        size = (8192, 8192)
         background = pygame.Surface(size)
         background = background.convert()
         background.fill((250, 250, 250))
@@ -23,15 +23,26 @@ class main():
         
         clock = pygame.time.Clock()
         
-        blocksize = 32 # This is important as it sets the base blocks all the maps are based on.
+        blocksize = 32  # This is important as it sets the base blocks all the maps are based on.
         
         player = Player(screen) # I added a playersprite, but it does nothing.
         
-        roomTiles, wallTiles = self.generate_room(blocksize, size)# This generates the map into two lists of sprites.
+        #roomTiles, wallTiles = self.generate_room(blocksize, size)# This generates the map into two lists of sprites.
         
+        #list = [wallTiles, roomTiles] # Here I put both lists into another, the position is important, as the second placement will override the first.
         
-        list = [wallTiles, roomTiles] # Here I put both lists into another, the position is important, as the second placement will override the first.
-        allsprites = pygame.sprite.LayeredDirty((list, player)) # Here I add the list to a DirtySprite class (does nothing atm)
+        fgroup = pygame.sprite.Group()
+        wgroup = pygame.sprite.Group()
+        
+        #testroom = Room((blocksize*20), (blocksize*20), 50, 50, blocksize, fgroup, wgroup)
+        
+        self.generate_room(blocksize, size, fgroup, wgroup)
+        print len(wgroup)
+        print len(fgroup)
+        #wallremove = pygame.sprite.groupcollide(wgroup, fgroup, True, False)
+        #print len(wgroup)
+        allsprites = pygame.sprite.LayeredUpdates(wgroup, fgroup)
+        
         run = 1
         
         while run == 1:
@@ -43,24 +54,27 @@ class main():
                     pygame.quit()
                 if event.type == KEYDOWN:
                     if event.key == K_w:
-                        ky += 50
+                        ky += 150
                     elif event.key == K_a:
-                        kx += 50
+                        kx += 150
                     elif event.key == K_s:
-                        ky -= 50
+                        ky -= 150
                     elif event.key == K_d:
-                        kx -= 50 
+                        kx -= 150 
+            
             
             allsprites.update()
+            
+        
             allsprites.draw(background) # Here we draw the map onto the background surface.
+            
             screen.blit(background, (kx,ky))
             pygame.display.update()
             allsprites.clear(screen, background)
             
+            
     def direction(self, rect, rooms, block, run, surf): #This methods checks each direction to see if there is space for another room there.
         size = surf
-        print size[0]
-        print size [1]
         dir = [1, 1, 1, 1]
         if rooms != []:
             if rect.centerx + (block*40) > size[0]: #checking right
@@ -152,121 +166,150 @@ class main():
 
                             
     # Generates a room rectangle of random size, and ties the other rects together.      
-    def generate_room(self, block, surf):
-        print surf[0]
+    def generate_room(self, block, surf, fg, wg):
         run = True
-        x = block
-        y = block
+        roomx, roomy = (block*2), (block*20)
+        hw = hh = rw = rh = 1000
         rooms = []
-        walls = []
-        rects = []
         dir = [0, 0, 0, 0]
+        while run:
+           
+           hw = random.randrange((block*4), (block*16), block)
+           hh = random.randrange((block*4), (block*16), block)
+           
+           hew = random.randrange((rw - rw),(rw - (block*4)), block)
+           heh = random.randrange((rh - rh), (rh - (block*4)), block)
+           if dir[0] == 1:#right
+                hh = block*2
+                roomy += heh
+                
+           elif dir[1] == 1:#left
+                hh = block*2
+                roomx -= hw
+                roomy += heh
+           elif dir[2] == 1:#up
+                hw = block*2
+                roomy -= hh
+                roomx += hew
+           elif dir[3] == 1:#down
+                hw = block*2
+                roomx += hew
+           else:
+               roomx += hw
+         
+           
+           hall = Room(hw, hh, roomx, roomy, block, fg, wg)
+           rooms.append(hall.rect)
+           
+           rw = random.randrange((block*8), (block*20), block)
+           rh = random.randrange((block*8), (block*20), block)
+           
+           hew = random.randrange((rw - rw),(rw - (block*4)), block)
+           heh = random.randrange((rh - rh), (rh - (block*4)), block)
+           
+           if dir[0] == 1:#right
+               roomx += hw 
+               roomy -= heh
+           elif dir[1] == 1:#left
+               roomx -= rw
+               roomy -= heh
+           elif dir[2] == 1:#up
+               roomy -= rh
+               roomx -= hew
+           elif dir[3] == 1:#down
+               roomy += hh
+               roomx -= hew
+                
+           room = Room(rw, rh, roomx, roomy, block, fg, wg)
+           rooms.append(room.rect)
+           
+           roomx, roomy, dir, run = self.direction(room.rect, rooms, block, run, surf)
+               
+     
         
-        while run == True:
-            hw = random.randrange((block*4), (block*12), block)
-            hh = random.randrange((block*4), (block*12), block)
-            
-            rw = random.randrange((block*8), (block*24), block)
-            rh = random.randrange((block*8), (block*24), block)
-            if dir[0] == 1:#right
-                hh = block*2
-            elif dir[1] == 1:#left
-                hh = block*2
-                x -= hw
-            elif dir[2] == 1:#up
-                hw = block*2
-                y -= hh
-            elif dir[3] == 1:#down
-                hw = block*2
-            myhall = pygame.Rect(x, y, hw, hh)
-            
-            if dir[0] == 1:#right
-                x += hw 
-            elif dir[1] == 1:#left
-                x -= rw
-            elif dir[2] == 1:#up
-                y -= rh
-            elif dir[3] == 1:#down
-                y += hh
-            myrect = pygame.Rect(x, y, rw, rh)
-            x, y, dir, run = self.direction(myrect, rects, block, run, surf) #Checks the directions
-            rects.append(myhall)
-            rects.append(myrect)
-            room_tiles, wall_tiles = self.sprite_map(myhall, block)
-            rooms.append(room_tiles)
-            walls.append(wall_tiles)
-            room_tiles, wall_tiles = self.sprite_map(myrect, block)
-            rooms.append(room_tiles)
-            walls.append(wall_tiles)
-        return rooms, walls
-    
+class Room(pygame.sprite.Sprite):
+    def __init__(self, w, h, x, y, block, fg, wg):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((w, h))
+        self.rect = pygame.Rect(x, y, w, h)
+        tiles, walls = self.sprite_map(block)
+        tiles, walls = self.wall_collide(tiles, walls)
+        fg.add(tiles)
+        wg.add(walls)
+        
+    def wall_collide(self, tiles, walls):
+        pygame.sprite.groupcollide(walls, tiles, True, False)
+        return tiles, walls
+        
+        
     # Here we make the rooms with floor tiles and wall tiles.
-    def sprite_map(self, rect, block):
-        surf = rect
-        row = ((surf.right - surf.left) / block)
-        col = ((surf.bottom - surf.top) / block)
-        fx = surf.left
-        fy = surf.top
-        wx = (surf.left - block)
-        wy = (surf.top - block)
+    def sprite_map(self, block):
+        surf = self.image
+        row = ((self.rect.right - self.rect.left) / block)
+        col = ((self.rect.bottom - self.rect.top) / block)
+        fx = self.rect.left
+        fy = self.rect.top
+        wx = (self.rect.left - (block))
+        wy = (self.rect.top - (block))
         
-        room_tiles = []
-        wall_tiles = []
-        for part in range(col):
-            for i in range(row):
+        room_tiles = pygame.sprite.Group()
+        wall_tiles = pygame.sprite.Group()
+        for part in xrange(col):
+            for i in xrange(row):
                 i = Tile(surf, block, 3)
                 i.rect.left = fx
                 i.rect.top = fy
-                room_tiles.append(i)
+                room_tiles.add(i)
                 fx += block
                 
             fy += block
-            fx = surf.left
-            
+            fx = self.rect.left
+        
+        
         for part in range(col + 2):
             for i in range(row + 2):
                 i = Tile(surf, block, 1)
                 i.rect.x = wx
                 i.rect.y = wy
-                wall_tiles.append(i)
+                wall_tiles.add(i)
                 wx += block
                 
             wy += block
-            wx = (surf.left - block)
-            
+            wx = (self.rect.left - block)
+                
         return room_tiles, wall_tiles
+
+
         
-        
+
+class Tile(pygame.sprite.DirtySprite): # The Tile sprite, this is one block in every room or wall, and we chose which tile we want.
+    def __init__(self, surf, blocksize, select):
+        pygame.sprite.DirtySprite.__init__(self)
+        self.area = surf
+        self.tile_select(blocksize, select)
     
-    
-                    
+    def tile_select(self, blocksize, select):
+        if select == 1:
+            self.image = pygame.image.load("stone.jpg").convert()
+            self.rect = self.image.get_rect()
+            self.area.blit(self.image, (self.rect.x, self.rect.y))
+        if select == 2:
+            self.image = pygame.image.load("wood.jpg").convert()
+            self.rect = self.image.get_rect()
+            self.area.blit(self.image, (self.rect.x, self.rect.y))
+        if select == 3:
+            self.image = pygame.image.load("blacktile.jpg").convert()
+            self.rect = self.image.get_rect()
+            self.area.blit(self.image, (self.rect.x, self.rect.y))
+            
+            
+
 class Player(pygame.sprite.DirtySprite): # Here we make the rooms with floor tiles and wall tiles.
     def __init__(self, screen):
         pygame.sprite.DirtySprite.__init__(self)
         self.image = pygame.image.load("Arrow_cursor.png").convert_alpha()
         self.rect = self.image.get_rect()
         self.area = screen
-        self.dirty = 1
-        
-        
-
-class Tile(pygame.sprite.DirtySprite): # The Tile sprite, this is one block in every room or wall, and we chose which tile we want.
-    def __init__(self, surf, blocksize, select):
-        pygame.sprite.DirtySprite.__init__(self)
-        self.tile_select(blocksize, select)
-        self.dirty = 1
-        self.area = surf
-    
-    def tile_select(self, blocksize, select):
-        if select == 1:
-            self.image = pygame.image.load("stone.jpg").convert()
-            self.rect = self.image.get_rect()
-        if select == 2:
-            self.image = pygame.image.load("wood.jpg").convert()
-            self.rect = self.image.get_rect()
-        if select == 3:
-            self.image = pygame.image.load("blacktile.jpg").convert()
-            self.rect = self.image.get_rect()
         
 if __name__ == '__main__': # To run.
     main()    
